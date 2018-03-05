d4e936fe692d577590344b99c94ce2af16ea79e3
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-03-05 23:41:00 +0100

    add dynamic port

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var socket = require('socket.io');
+var https = require('https');
 
 var app = express();
 
-server = app.listen(5000, function(){
+server = app.listen(process.env.PORT, function(){
     console.log('server is running on port 5000')
 });
 
 io = socket(server);
 
 io.on('connection', (socket) => {
-    console.log(socket.id);
+    // console.log(socket.id);
 
     socket.on('SEND_MESSAGE', function(data){
         //console.log(data);
